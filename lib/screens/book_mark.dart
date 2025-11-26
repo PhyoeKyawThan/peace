596b43f96a145b238cc83f1bb6af740836913aef
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-11-27 01:20:26 +0630

    remove: unnecessary import

--- lib/screens/book_mark.dart
+++ lib/screens/book_mark.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mind_peace/constants/colors.dart';
-import 'package:mind_peace/dialogs/lesson_item_dialog.dart';
-import 'package:mind_peace/models/dialog_model.dart';
 import 'package:mind_peace/models/lesson_model.dart';
 import 'package:mind_peace/screens/single_lesson_view.dart';
 import 'package:mind_peace/services/audio_service.dart';
